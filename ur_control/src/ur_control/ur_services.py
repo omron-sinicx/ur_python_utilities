b8fd1dfdbde9b7b708a8b2cdd06b274aa7e73113
Cristian Beltran <cristianbehe@gmail.com>
2023-12-01 15:38:38 +0900

    Fix UR activate sequence

--- ur_control/src/ur_control/ur_services.py
+++ ur_control/src/ur_control/ur_services.py
@@ -1,38 +1,3 @@
-#!/usr/bin/env python
-
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2021, OMRON SINIC X
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of OMRON SINIC X nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Author: Cristian C. Beltran-Hernandez, Felix von Drigalski
 
 import rospy
 import time
@@ -228,18 +193,17 @@ class URServices():
             rospy.logdebug("Try to quit before connecting.")
             response = self.ur_dashboard_clients["quit"].call()
             rospy.sleep(1)
+            rospy.logdebug("Try to connect to dashboard service.")
+            response = self.ur_dashboard_clients["connect"].call()
+            rospy.sleep(1)
             if response.success:
-                rospy.logdebug("Try to connect to dashboard service.")
-                response = self.ur_dashboard_clients["connect"].call()
+                rospy.logdebug("Try to stop program.")
+                response = self.ur_dashboard_clients["stop"].call()
                 rospy.sleep(1)
                 if response.success:
-                    rospy.logdebug("Try to stop program.")
-                    response = self.ur_dashboard_clients["stop"].call()
+                    rospy.logdebug("Try to play program.")
+                    response = self.ur_dashboard_clients["play"].call()
                     rospy.sleep(1)
-                    if response.success:
-                        rospy.logdebug("Try to play program.")
-                        response = self.ur_dashboard_clients["play"].call()
-                        rospy.sleep(1)
         except:
             rospy.logwarn("Dashboard service did not respond! (2)")
             pass
