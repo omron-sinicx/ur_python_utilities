40fc34c67d688abd95dfa7e4c27f4958fda2c5b0
Cristian C Beltran-Hernandez <cristianbehe@gmail.com>
2024-01-30 16:02:43 +0900

    Fix installation

--- ur_pykdl/setup.py
+++ ur_pykdl/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup()
-d['packages'] = ['ur_pykdl', 'ur_kdl', 'urdf_parser_py']
+d['packages'] = ['ur_pykdl', 'ur_kdl']
 d['package_dir'] = {'': 'src'}
 
 setup(**d)
